a4d511e900b9f2a60731c5e666254b4a99423da0
Edlan So <edlanso2005@gmail.com>
2025-11-14 15:08:10 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Edlan-So-Portfolio
--- postbuild.mjs
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-const distDir = path.resolve("dist");
-const source404 = path.resolve("404.html");
-const target404 = path.join(distDir, "404.html");
-
-fs.copyFileSync(source404, target404);
-console.log("✅ 404.html copied to dist/");
